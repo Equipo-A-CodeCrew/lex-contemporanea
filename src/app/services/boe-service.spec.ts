d906c69e2ad91e629136b6c90842df028e3fa1a4
Andrés Aragón <163292512+aragonaj@users.noreply.github.com>
2026-01-22 12:45:18 +0000

    Añadidos los test para el servicio del BOE

--- src/app/services/boe-service.spec.ts
+++ src/app/services/boe-service.spec.ts
@@ -1,16 +1,27 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
 
 import { BoeService } from './boe-service';
+import { environment } from '../utils/environment';
 
 describe('BoeService', () => {
   let service: BoeService;
+  let http: HttpClient;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [BoeService, provideHttpClient()]
+    });
+
     service = TestBed.inject(BoeService);
+    http = TestBed.inject(HttpClient);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('getSumario should call HttpClient.get with baseUrl', () => {
+    const spy = spyOn(http, 'get').and.returnValue({} as any);
+
+    service.getSumario();
+
+    expect(spy).toHaveBeenCalledWith(environment.boeApiUrl);
   });
 });
