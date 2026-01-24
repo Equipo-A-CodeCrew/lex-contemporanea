37b9b2de0077059708249e8f1dfc21c52f2d5ba0
Andrés Aragón <163292512+aragonaj@users.noreply.github.com>
2026-01-24 07:22:02 +0000

    Se modificó la fecha de obtención de datos temporalmente

--- src/app/components/default-filters/default-filters.ts
+++ src/app/components/default-filters/default-filters.ts
@@ -18,9 +18,19 @@ export class DefaultFilters {
     private readonly filtersService: DefaultFiltersService
   ) { }
 
+  // Código temporalmente comentado para mayor comodidad
+  // ngOnInit(): void {
+  //   const today = new Date();
+  //   const date = `${today.getFullYear()}${String(today.getMonth() + 1).padStart(2, '0')}${String(today.getDate()).padStart(2, '0')}`;
+
+  //   this.boeService.getSumario(date).subscribe(data => {
+  //     console.log('SUMARIO RAW', data);
+  //     this.sumario = data;
+  //   });
+  // }
+
   ngOnInit(): void {
-    const today = new Date();
-    const date = `${today.getFullYear()}${String(today.getMonth() + 1).padStart(2, '0')}${String(today.getDate()).padStart(2, '0')}`;
+    const date = '20260115'; // fecha provisional
 
     this.boeService.getSumario(date).subscribe(data => {
       console.log('SUMARIO RAW', data);
